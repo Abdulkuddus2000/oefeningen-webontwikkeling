4fdd9383cfb1fef53a396abc1de2bb9d98a6a39e
Abdul <42711396+Abdulkuddus2000@users.noreply.github.com>
2025-03-27 16:55:02 +0000

    Table of persons

--- theorie/demoExpress/index.ts
+++ theorie/demoExpress/index.ts
@@ -2,18 +2,23 @@ import express from "express";
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", "./views");
 app.set("port", 3000);
 
+interface Person{
+    name: string;
+    city: string;
+    age: number;
+}
+
 app.get("/", (req, res) => {
-    let randomGetal: number = Math.random()*100;
-    let randomGetal2: number = randomGetal * 2;
-    res.render("index", 
-        {
-            aRandomNumber: randomGetal,
-            name: "Sven",
-            age: 40,
-            someOtherNumber: randomGetal2
-        });
+    let persons: Person[] = [
+        {name: "Sven", city: "Antwerpen", age: 40},
+        {name: "Andie", city: "Gent", age: 30},
+        {name: "Sam", city: "Brussel", age: 25},
+        {name: "Barbara", city: "Leuven", age: 35}
+    ];
+    res.render("index", {persons: persons});
 });
 
 app.listen(app.get("port"), () => console.log("[server] http://localhost:" +app.get("port")));
