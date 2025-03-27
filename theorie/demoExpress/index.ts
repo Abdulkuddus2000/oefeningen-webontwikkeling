3f98db0e25de85e2983373426a06f800c05644ca
Abdul <42711396+Abdulkuddus2000@users.noreply.github.com>
2025-03-27 16:02:03 +0000

    Math.Random

--- theorie/demoExpress/index.ts
+++ theorie/demoExpress/index.ts
@@ -5,11 +5,8 @@ app.set("view engine", "ejs");
 app.set("port", 3000);
 
 app.get("/", (req, res) => {
-    res.render("index");
-});
-
-app.get("/info", (req, res) => {
-    res.render("info");
+    let randomGetal: number = Math.random()*100;
+    res.render("index", {aRandomNumber: randomGetal});
 });
 
 app.listen(app.get("port"), () => console.log("[server] http://localhost:" +app.get("port")));
