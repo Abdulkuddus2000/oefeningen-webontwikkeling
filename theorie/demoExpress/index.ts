5dee0c87b4b5adfdb2718c5c54d6d9b56c3dea09
Abdul <42711396+Abdulkuddus2000@users.noreply.github.com>
2025-03-27 15:36:22 +0000

    mijn eerste html pagina in ejs

--- theorie/demoExpress/index.ts
+++ theorie/demoExpress/index.ts
@@ -1,15 +1,11 @@
 import express from "express";
-
 const app = express();
 
+app.set("view engine", "ejs");
 app.set("port", 3000);
 
 app.get("/", (req, res) => {
-    let randomGetal = Math.random()*100;
-    res.type("text/html");
-    res.send(`Het random getal is ${randomGetal}`);
+    res.render("index");
 });
 
-app.listen(app.get("port"), () => 
-    console.log("[server] http://localhost:" +app.get("port"))
-);
+app.listen(app.get("port"), () => console.log("[server] http://localhost:" +app.get("port")));
