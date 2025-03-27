bc9d70826b4e8168ff94429cd95735ee079cef9f
Abdul <42711396+Abdulkuddus2000@users.noreply.github.com>
2025-03-27 16:20:18 +0000

    Message of John Smith

--- theorie/demoExpress/index.ts
+++ theorie/demoExpress/index.ts
@@ -6,7 +6,14 @@ app.set("port", 3000);
 
 app.get("/", (req, res) => {
     let randomGetal: number = Math.random()*100;
-    res.render("index", {aRandomNumber: randomGetal});
+    let randomGetal2: number = randomGetal * 2;
+    res.render("index", 
+        {
+            aRandomNumber: randomGetal,
+            name: "Sven",
+            age: 40,
+            someOtherNumber: randomGetal2
+        });
 });
 
 app.listen(app.get("port"), () => console.log("[server] http://localhost:" +app.get("port")));
