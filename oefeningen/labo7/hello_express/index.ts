291e2a50c398be0b615c7d53498d8916bb8da6e1
Abdul Kosimov <abdul.kosimov@student.ap.be>
2025-04-21 21:22:34 +0000

    oefeningen van hello express afgemaakt

--- oefeningen/labo7/hello_express/index.ts
+++ oefeningen/labo7/hello_express/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
+import { Pokemon, PokemonSprites } from "./public/types";
 
 dotenv.config();
 
@@ -59,16 +60,79 @@ app.get("/whoamijson", (req, res) => {
 const pikachu = fetch("https://pokeapi.co");
 
 
-app.get("/pikachujson", async(req, res) => {
-    const response = await fetch("https://pokeapi.co/api/vs/pokemon/pikachu");
-    const pikachu: Pokemon
-    res.json(pikachu);
+app.get("/pikachu_json", async(req, res) => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+    const data = await response.json();
 
+    const pikachu: Pokemon = data;
+    res.json(pikachu);
 });
 
-app.get("/pikachuhtml", async (req, res) => {
-    const response = await fetch("https://pokeapi.co/api/vs/pokemon/pikachu");
+app.get("/pikachu_html", async (req, res) => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+    const data = await response.json();
 
+    const pikachu: Pokemon = data;
+    const html = `
+        <html>
+            <body>
+                <h1>Naam: ${pikachu.name}</h1>
+
+                <p>Id: ${pikachu.id}</p>
+                <p>Gewicht: ${pikachu.weight}</p>
+                <img src="${pikachu.sprites.front_default}">
+                <img src="${pikachu.sprites.back_default}">
+            </body>
+        </html>
+    `;
+
+    res.send(html);
+});
+
+
+app.get("/randomcolor", (req, res) => {
+    const letters = ["A", "B", "C", "D", "E", "F"];
+    const randomColor: string[] = [];
+
+    for (let i = 0; i < 6; i++) {
+        const randomNumber = Math.floor(Math.random() * 16);
+        
+        if (randomNumber > 9) {
+            randomColor[i] = letters[randomNumber - 10];
+        } else {
+            randomColor[i] = randomNumber.toString();
+        }
+    }
+    
+    let usableColor: string = "#" + randomColor.join("");
+
+    res.type("text/html");
+    const html = `
+        <html>
+            <head>
+                <style>
+                    body{
+                        background-color: ${usableColor};
+                    }
+
+                    h1 {
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -50%);
+                        font-family: sans-serif;
+                        font-size: 3rem;
+                    }
+                </style>
+            </head>
+
+            <body>
+                <h1>${usableColor}</h1>
+            </body>
+        </html>    
+    `;
+
+    res.send(html);
 });
 
 app.listen(app.get("port"), () => {
