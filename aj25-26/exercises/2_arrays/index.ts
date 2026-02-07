66608d53377c688aa1d7ea1cb8766e1f52d5ea02
Abdul Kosimov <adkosimov@gmail.com>
2026-02-07 20:38:29 +0000

    labo 2 - tot oefening 3 - Rot13

---
+++ aj25-26/exercises/2_arrays/index.ts
@@ -0,0 +1,89 @@
+// oefening 1 - som van getallen
+
+import * as rl from 'readline-sync';
+
+// oefening 1 - som van getallen
+
+// let amount: number = rl.questionInt("Hoeveel getallen wil je optellen? ");
+// let myArray: number[] = new Array(amount);
+
+// for (let i = 0; i < amount; i++) {
+//     myArray[i] = rl.questionInt(`Geef getal ${i + 1} in: `);
+// }
+
+// let sum: number = 0;
+
+// for (let i = 0; i < myArray.length; i++) {
+//     sum += myArray[i];
+// }
+
+// console.log(sum);
+
+// oefening 2 - puntenboek
+// let counter: number = 1; // start met 1
+// let counter_low_grades: number = 0;
+// let gradeBook: number[] = []; // start leeg
+// let input: string;
+
+// while (true) {
+//     input = rl.question(`Geef het punt van student ${counter} in: `);
+//     if (input.trim() != "") {
+//         gradeBook.push(Number(input));
+//         counter++;
+//         if (Number(input) < 10) {
+//             counter_low_grades++;
+//         }
+//         continue;
+//     } else {
+//         let sum = 0;
+//         let average: number = 0;
+//         for (let i = 0; i < gradeBook.length; i++) {
+//             sum += gradeBook[i];
+//         }
+
+//         average = sum / gradeBook.length;
+//         console.log(`average: ${average}, number of low graders: ${counter_low_grades}`);
+//         break;
+//     }
+// }
+
+// oefening 3 - Rot13
+// let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// let input: string = rl.question("Gimme some txt bro: ");
+// let newIndex: number;
+// let newWord: string[] = new Array(input);
+
+// for (let i = 0; i < input.length; i++) { // itereer over het ingegeven woord
+//     // console.log(input[i]);
+//     // search in the alphabet
+//     for (let j = 0; j < alphabet.length; j++) { // itereer over het alfabet
+//         if (input[i] == alphabet[j]) { // als letter van ingegeven woord overeenkomt met het huidige letter van het alfabet
+//             newIndex = (alphabet.indexOf(alphabet[j].toLowerCase()) + 13) % 26;
+//             newWord[j] += alphabet[newIndex];
+//         }
+//     }
+// }
+
+// console.log(newWord);
+
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+let input: string = rl.question("Enter a string: ");
+let result: string = "";
+
+for (let i = 0; i < input.length; i++) {
+    let char = input[i].toLocaleLowerCase();
+    
+    let index = alphabet.indexOf(char);
+
+    if (index != -1) {
+        let newIndex = (index + 13) % 26;
+        result += alphabet[newIndex];
+    } else {
+        result += input[i];
+    }
+}
+
+console.log(result);
+
+export {}
