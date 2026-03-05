58b84639ec7e03a469bba9167908d0c7b2516471
Abdul Kosimov <adkosimov@gmail.com>
2026-03-05 07:52:24 +0000

    theorie arrays - nog niet af

---
+++ aj25-26/theory/l3_arrays/index.ts
@@ -0,0 +1,17 @@
+// basic types
+
+console.log(`1 / 0 = ${1 / 0}`);
+
+// number omzetten naar string
+let number: number = 4;
+let str: string = number.toString();
+
+console.log(typeof number);
+console.log(typeof str);
+
+// null values
+let collegeDegree = null;
+
+// undefined
+
+
